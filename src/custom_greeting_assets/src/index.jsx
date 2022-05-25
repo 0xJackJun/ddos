7e65d6d55ad272e7c92579aefb372f4975dac17f
quanrong <jackqr1996@gmail.com>
2022-05-25 20:38:54 +0800

    add data convert

--- src/custom_greeting_assets/src/index.jsx
+++ src/custom_greeting_assets/src/index.jsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { service } from "../../declarations/service";
+import { Principal } from "@dfinity/principal";
 
-const MyHello = () => {
-  const [name, setName] = React.useState('');
+const Create = () => {
+  const [canisterId, setCanisterId] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [threshold, setThreshold] = React.useState('');
   const [message, setMessage] = React.useState('');
 
-  async function doGreet() {
-    const greeting = await service.create(name);
-    setMessage(greeting);
+  async function createUser() {
+    const greeting = await service.create({ "threshold": threshold, canister_id: canisterId, "email": email });
+    setMessage("succeeded");
   }
 
   return (
@@ -16,18 +19,27 @@ const MyHello = () => {
       <div style={{ "backgroundColor": "yellow" }}>
         <p>Dfinity DDos Defender</p>
         <p>
-          {" "}
-          Please Input Your threshold, canister_id, email, and click{" "}
+          Please Input Your threshold, canister_id, email, and click
           <b> Submit</b> to display the result.
         </p>
       </div>
       <div style={{ margin: "30px" }}>
         <input
-          id="name"
-          value={name}
-          onChange={(ev) => setName(ev.target.value)}
+          id="threshold"
+          value={threshold}
+          onChange={(ev) => setThreshold(ev.target.value)}
         ></input>
-        <button onClick={doGreet}>Submit!</button>
+        <input
+          id="canister_id"
+          value={canisterId}
+          onChange={(ev) => setCanisterId(Principal.fromText(ev.target.value))}
+        ></input>
+        <input
+          id="email"
+          value={email}
+          onChange={(ev) => setEmail(ev.target.value)}
+        ></input>
+        <button onClick={createUser}>Submit!</button>
       </div>
       <div>
         Result is: "
@@ -37,4 +49,4 @@ const MyHello = () => {
   );
 };
 
-render(<MyHello />, document.getElementById("app"));
+render(<Create />, document.getElementById("app"));
